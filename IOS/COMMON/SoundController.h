74bbc1ff18f2622a00b5c91e95a243a1277a93f2
Nguyen The Binh <binhngt@elcom.com.vn>
2015-09-14 09:21:11 +0700

    common
    all file to use in common

---
+++ IOS/COMMON/SoundController.h
@@ -0,0 +1,25 @@
+//
+//  SoundController.h
+//  100Numbers
+//
+//  Created by Nguyễn Thế Bình on 4/2/15.
+//  Copyright (c) 2015 LapTrinhAlgo.Com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SoundController : NSObject
+{
+    
+}
+
++(instancetype) GetSingleton;
+- (void) PlayClickButton;
+- (void) PlaySoundGameOver;
+- (void) PlaySoundCongratulation;
+- (void) PlaySoundCorrect;
+- (void) PlaySoundFail;
+- (void) ChangeMute;
+- (BOOL) GetMute;
+- (void) ShowSpeaker:(UIButton*) p_speaker;
+@end
