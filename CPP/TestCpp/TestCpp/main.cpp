862fb90c7cb3683945fad2e670dc7defd308c8f7
Nguyen The Binh <thatdaycoming@gmail.com>
2017-02-16 15:01:20 +0700

    Fist project cpp

---
+++ CPP/TestCpp/TestCpp/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  TestCpp
+//
+//  Created by Nguyen The Binh on 2/16/17.
+//  Copyright © 2017 Nguyen The Binh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
